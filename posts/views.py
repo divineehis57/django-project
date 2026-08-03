f3e15a4df9ad5d223c0981d957caeb82152c2d07
divineehis57 <divineehis57@gmail.com>
2026-08-03 02:41:46 +0100

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,22 @@
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from config.permissions import IsAuthorOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.parsers import MultiPartParser, FormParser
+
+
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related("user").all().order_by("created_at")
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    # parser_classes = [MultiPartParser, FormParser]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields= ["title"]
+    search_fields = ["title", "contents"]
+    ordering_fields = ["created_at"]
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
